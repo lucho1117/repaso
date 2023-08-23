3eaa9445304e420e9c891b8b50e973ba68ed2dd1
chumillucio <chumillucio@gmail.com>
2023-08-23 14:55:07 -0600

    +angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,25 @@ import { AppComponent } from './app.component';
 import { MasterDetailComponent } from './pages/master-detail/master-detail.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { FacturaComponent } from './pages/factura/factura.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterDetailComponent,
     PrincipalComponent,
-    FacturaComponent
+    FacturaComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
