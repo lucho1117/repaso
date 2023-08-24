c2f280b707e1f9810e87acc030729f0fbf567b8a
chumillucio <chumillucio@gmail.com>
2023-08-23 22:27:19 -0600

    +resolucion de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { MasterDetailComponent } from './pages/master-detail/master-detail.component';
 import { FacturaComponent } from './pages/factura/factura.component';
-import { DenunciaComponent } from './pages/denuncia/denuncia.component';
 import { ROUTER_DENUNCIA } from './pages/denuncia/denuncia-routing.module'
 
 const routes: Routes = [
   { path: 'home', component: PrincipalComponent },
   { path: 'prototipo', component: MasterDetailComponent },
   { path: 'factura', component: FacturaComponent},
-  { path: 'denuncia', component: DenunciaComponent, children: ROUTER_DENUNCIA},
+  { path: 'denuncia', children: ROUTER_DENUNCIA},
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/pages/denuncia/denuncia-routing.module.ts
+++ src/app/pages/denuncia/denuncia-routing.module.ts
@@ -1,15 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { FormDenunciaComponent } from './form-denuncia/form-denuncia.component';
+import { DenunciaComponent } from './denuncia.component';
 
 export const ROUTER_DENUNCIA: Routes = [
+  { path: '', component: DenunciaComponent, },
   { path: 'form-denuncia', component: FormDenunciaComponent },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
-/* export const APP_ROUTING = RouterModule.forRoot(ROUTER_DENUNCIA); */
-
-/* @NgModule({
-  imports: [RouterModule.forRoot(ROUTER_DENUNCIA)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { } */
